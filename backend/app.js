cf8ba2bb2cbcb04e3ed42c4a26150ce87dd58ace
vipin_760 <vipinm500@gmail.com>
2024-05-19 20:18:16 +0530

    added new url

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const errorMiddleware = require('./middleware/error');
 
 app.use(express.json());
 app.use(cors({
-    origin:["http://localhost:5173"],
+    origin:["taksmanage.netlify.app"],
     credentials:true
 }));
 
